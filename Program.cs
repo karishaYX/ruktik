9f533c18512f89004f06caf9384514d5f389ea33
karishaYX <furskarisha@gmail.com>
2025-05-30 23:07:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection.Metadata.Ecma335;
 
 namespace MazeGame
 {
@@ -8,184 +7,141 @@ namespace MazeGame
         static int mazeWidth = 20;
         static int mazeHeight = 20;
         static char[,] maze = new char[mazeHeight, mazeWidth];
-        static int playerX;
-        static int playerY;
-        static int exitX;
-        static int exitY;
+        static int playerX, playerY;
+        static int exitX, exitY;
         static Random rand = new Random();
 
         static void Main(string[] args)
         {
-            // InitializeMaze();
             while (true)
             {
+                InitializeMaze(); 
                 bool gameRunning = true;
+
                 while (gameRunning)
                 {
                     Console.Clear();
-                    // DrawMaze();
+                    DrawMaze();
 
-                    Console.WriteLine("Цель:найти выход (обозначен'E').");
-                    Console.WriteLine("Управление:W - вверх,S - вниз,A - влево,D - вправо.");
-                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                    int newX = playerX;
-                    int newY = playerY;
+                    Console.WriteLine("Цель: найти выход (E). Управление: W, A, S, D.");
+                    var key = Console.ReadKey(true).Key;
 
-                    switch (keyInfo.Key)
+                    
+                    int newX = playerX, newY = playerY;
+                    switch (key)
                     {
-                        case ConsoleKey.W:
-                            newY--;
-                            break;
-                        case ConsoleKey.S:
-                            newY++;
-                            break;
-                        case ConsoleKey.A:
-                            newX--;
-                            break;
-                        case ConsoleKey.D:
-                            newX++;
-                            break;
-                        default:
-                            continue;
+                        case ConsoleKey.W: newY--; break;
+                        case ConsoleKey.S: newY++; break;
+                        case ConsoleKey.A: newX--; break;
+                        case ConsoleKey.D: newX++; break;
+                        case ConsoleKey.Escape: return; 
+                        default: continue;
                     }
-                    if (newX >= 0 && newX < mazeWidth && newY >= 0 && newY < mazeHeight)
-                    {
-                        if (maze[newY, newX] != '#')
-                        {
-                            playerX = newX;
-                            playerY = newY;
-                            if (playerX == exitX && playerY == exitY)
-                            {
-                                Console.Clear();
-                                // DrawMaze();
-                                Console.WriteLine("Конец");
-                                Console.ReadKey();
-                                // InitializeMaze();
-                                gameRunning = false;
-                            }
 
+                    
+                    if (newX >= 0 && newX < mazeWidth && newY >= 0 && newY < mazeHeight && maze[newY, newX] != '#')
+                    {
+                        playerX = newX;
+                        playerY = newY;
+
+                        
+                        if (playerX == exitX && playerY == exitY)
+                        {
+                            Console.Clear();
+                            DrawMaze();
+                            Console.WriteLine("Победа!УРАААААА");
+                            Console.ReadKey();
+                            gameRunning = false;
                         }
                     }
                 }
             }
         }
+
         static void InitializeMaze()
         {
-            // for (int y = 0; y < mazeHeight; y++)
+           
+            for (int y = 0; y < mazeHeight; y++)
+                for (int x = 0; x < mazeWidth; x++)
+                    maze[y, x] = '#';
+
+           
+            GenerateMaze(1, 1);
+
+          
+            do
             {
-                // for (int x = 0; x < mazeWidth; x++)
+                playerX = rand.Next(1, mazeWidth - 1);
+                playerY = rand.Next(1, mazeHeight - 1);
+            } while (maze[playerY, playerX] != ' ');
+
+          
+            SetExit();
+        }
+
+        static void GenerateMaze(int x, int y)
+        {
+            maze[y, x] = ' ';
+            int[] dirs = { 0, 1, 2, 3 };
+            Shuffle(dirs);
+
+            foreach (int dir in dirs)
+            {
+                int nx = x, ny = y;
+                switch (dir)
                 {
-                    // maze[y, x] = '#';
+                    case 0: ny -= 2; break; 
+                    case 1: nx += 2; break; 
+                    case 2: ny += 2; break; 
+                    case 3: nx -= 2; break; 
+                }
+
+                if (nx > 0 && nx < mazeWidth - 1 && ny > 0 && ny < mazeHeight - 1 && maze[ny, nx] == '#')
+                {
+                    maze[(ny + y) / 2, (nx + x) / 2] = ' '; 
+                    GenerateMaze(nx, ny);
                 }
             }
-            // GenerateMaze(1, 1);
-            // playerX = 1;
-            // playerY = 1;
-            // SetExit();
         }
 
-      //static void GenerateMaze(int x, int y);
-      {
-          //maze[y, x] = ' ';
-
-          int[] directions = { 0, 1, 2, 3, };
-        //Shiffle(directions);
-        //foreach (int dir in directions)
+        static void Shuffle(int[] array)
         {
-        //int nx = x;
-        //int ny = y;
-        // switch(dir)
-        { 
-        //case 0: ny -= 2; break;
-        //case 1: nx += 2; break;
-        //case 2: ny += 2; break;
-        //case 3: nx -= 2; break;
-    }
-    //if (nx > 0 && nx < mazeWidth - 1 && ny > 0 && ny < mazeHeight - 1)
-   {
-      // if (maze[ny, nx] == '#')
-      {
-        // maze[(ny + y) / 2, (nx + x) / 2] = ' ';
-        //GenerateMaze(nx, ny);
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
         }
-}
-}
-}
- static void Shuffle(int[] array)
- {
-    //for (int i = Array.Length - 1; i > 0; i--)
-    {
-        //int j = rand.Next(i + 1);
-        //int temp = Array[i];
-        //Array[i] = Array[j];
-        //aaray[j] = temp;
-    }
-}
-
-static void SetExit()
-{
-    bool placed = false;
-
-    while (!placed)
-    {
-        int side = rand.Next(4);
-        switch (side)
+        static void SetExit()
         {
-            case 0:
-            //exitX = rand.Next(1, mazeWidth - 2);
-            //exitY = 0;
-            //break;
-            case 1:
-            //exitX = mazeWidth - 1;
-            //exitY = rand.Next(1, mazeHeight - 2);
-            //break;
-            case 2:
-            //exitX = rand.Next(1, mazeWidht - 2);
-            //exitY = mazeHeight - 1;
-            //break;
-            case3:
-                //exitX = 0;
-                //exitY = rand.Next(1, mazeHeight - 2);
-                break;
-        }
-
-        if (maze[exitY, exitX] != '#')
-        {
-            maze[exitY, exitX] = 'E';
-            placed = true;
-        }
-    }
-}
-static void DrawMaze()
-{
-    for (int y = 0; y < mazeHeight; y++)
-    {
-        for (int x = 0; x < mazeWidth; x++)
-        {
-            if (x == playerX && y == playerY)
-                Console.Write('@');
-            else
-                Console.Write(maze[y, x]);
-        }
-        Console.WriteLine();
-    }
-}
-
-}
-}
-
-
-
-
-
-
-
-
-  
-    
             
-         
-     
+            int side = rand.Next(4);
+            switch (side)
+            {
+                case 0: exitX = rand.Next(1, mazeWidth - 1); exitY = 0; break;               
+                case 1: exitX = mazeWidth - 1; exitY = rand.Next(1, mazeHeight - 1); break;  
+                case 2: exitX = rand.Next(1, mazeWidth - 1); exitY = mazeHeight - 1; break;  
+                case 3: exitX = 0; exitY = rand.Next(1, mazeHeight - 1); break;              
+            }
+            maze[exitY, exitX] = 'E';
+        }
+
+        static void DrawMaze()
+        {
+            for (int y = 0; y < mazeHeight; y++)
+            {
+                for (int x = 0; x < mazeWidth; x++)
+                {
+                    if (x == playerX && y == playerY) Console.Write('@');
+                    else Console.Write(maze[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
 
 
 
